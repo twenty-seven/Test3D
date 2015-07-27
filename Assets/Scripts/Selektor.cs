0faa3f060bef83d53f51b2dff4f187a91cc807b0
Alex M <meerhoff.a@gmail.com>
2015-07-27 10:30:34 +0200

    Added selector to visualize mouse and possible unit selection

---
+++ Assets/Scripts/Selektor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BlobWars {
+	public class Selektor : MonoBehaviour {
+
+		//to allow selection of object in editor
+		public GameObject selectObject;
+
+
+		private GameObject selector;
+
+		// Use this for initialization
+		void Start () {
+			selector = (GameObject)Instantiate (selectObject, transform.position, Quaternion.identity);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			//get mouse position on plane and hit
+			if (Physics.Raycast (ray, out hit)) {
+				Blob b = hit.transform.GetComponent<Blob> ();
+				if (b != null && b.tower != null) {
+					selector.transform.position = new Vector3(b.transform.position.x ,20 , b.transform.position.z);
+				} else {
+					selector.transform.position = new Vector3(hit.point.x, 2, hit.point.z);
+				}
+			}
+
+		}
+	}
+}
